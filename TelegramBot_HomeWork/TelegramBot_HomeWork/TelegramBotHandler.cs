213db1f2a98597e5f3b46a45e850ce15ec34bc8c
Daniil Lukashin <39368626+GameShowLDA@users.noreply.github.com>
2024-10-21 23:29:26 +0400

    Новый метод возвращения Роли и обработки сообщений

--- TelegramBot_HomeWork/TelegramBot_HomeWork/TelegramBotHandler.cs
+++ TelegramBot_HomeWork/TelegramBot_HomeWork/TelegramBotHandler.cs
@@ -65,25 +65,22 @@ namespace TelegramBot
     {
       if (update.Type == UpdateType.Message && update.Message?.Text != null)
       {
-        await HandleMessageAsync(update.Message, cancellationToken);
+        await HandleMessageAsync(botClient, update.Message, cancellationToken);
       }
       else if (update.Type == UpdateType.CallbackQuery)
       {
-        await HandleCallbackQueryAsync(update.CallbackQuery, cancellationToken);
+        await HandleCallbackQueryAsync(botClient, update.CallbackQuery, cancellationToken);
       }
     }
 
     #region Обработка сообщений.
 
-    private async Task HandleMessageAsync(Message message, CancellationToken cancellationToken)
+    private async Task HandleMessageAsync(ITelegramBotClient botClient, Message message, CancellationToken cancellationToken)
     {
       var chatId = message.Chat.Id;
       var messageText = message.Text;
 
-      //TODO : Позже раскоментить
-      //UserRole? userRole = CommonDataModel.GetUserRoleById(chatId);
-      UserRole userRole = UserRole.Teacher;
-
+      UserRole? userRole = CommonUserModel.GetUserRoleById(chatId);
       bool responseMessage = userRole switch
       {
         UserRole.Administrator => await HandleAdministratorMessageAsync(chatId, messageText),
@@ -94,7 +91,8 @@ namespace TelegramBot
 
       if (!responseMessage)
       {
-        await _botClient.SendTextMessageAsync(chatId, "Ошибка: Данные пользователя не найдены!");
+        // TODO : Сделать регистрацию...
+        await SendMessageAsync(botClient, chatId, "Ошибка: Данные пользователя не найдены!");
       }
     }
 
@@ -124,12 +122,10 @@ namespace TelegramBot
     /// <returns>Ответ на сообщение учителя.</returns>
     private async Task<bool> HandleTeacherMessageAsync(long chatId, string message)
     {
-      //TODO : Позже раскоментить
-      //var userData = CommonDataModel.GetUserById(chatId);
-      var userData = new UserModel(chatId, "TestName", "TestLastname", "email@mail.ru", UserRole.Teacher);
+      var userData = CommonUserModel.GetUserById(chatId);
       if (userData != null)
       {
-        var teacher = new Teacher (userData.TelegramChatId, userData.FirstName, userData.LastName, userData.Email);
+        var teacher = new Teacher(userData.TelegramChatId, userData.FirstName, userData.LastName, userData.Email);
         await teacher.ProcessMessageAsync(_botClient, chatId, message);
         return true;
       }
@@ -144,9 +140,7 @@ namespace TelegramBot
     /// <returns>Ответ на сообщение студента.</returns>
     private async Task<bool> HandleStudentMessageAsync(long chatId, string message)
     {
-      //TODO : Позже раскоментить
-      // var userData = CommonDataModel.GetUserById(chatId);
-      var userData = new UserModel(chatId, "TestName", "TestLastname", "email@mail.ru", UserRole.Student);
+      var userData = CommonUserModel.GetUserById(chatId);
       if (userData != null)
       {
         var student = new Student(userData.TelegramChatId, userData.FirstName, userData.LastName, userData.Email);
@@ -166,13 +160,17 @@ namespace TelegramBot
     /// </summary>
     /// <param name="callbackQuery">Данные callback-запроса.</param>
     /// <param name="cancellationToken">Токен отмены операции.</param>
-    private async Task HandleCallbackQueryAsync(CallbackQuery callbackQuery, CancellationToken cancellationToken)
+    private async Task HandleCallbackQueryAsync(ITelegramBotClient botClient, CallbackQuery callbackQuery, CancellationToken cancellationToken)
     {
       var chatId = callbackQuery.From.Id;
 
-      //TODO : Позже раскоментить
-      //UserRole? userRole = CommonDataModel.GetUserRoleById(chatId);
-      UserRole userRole = UserRole.Teacher;
+      UserRole? userRole = CommonUserModel.GetUserRoleById(chatId);
+
+      if (userRole is null)
+      {
+        await _botClient.SendTextMessageAsync(chatId, "Ошибка: Данные пользователя не найдены!");
+        return;
+      }
 
       bool responseMessage = userRole switch
       {
@@ -184,8 +182,9 @@ namespace TelegramBot
 
       if (!responseMessage)
       {
-        await _botClient.SendTextMessageAsync(chatId, "Ошибка: Данные пользователя не найдены!");
+        await SendMessageAsync(botClient, chatId, "Ошибка при обработке запроса.");
       }
+
     }
 
     /// <summary>
@@ -214,9 +213,7 @@ namespace TelegramBot
     /// <returns>Ответ на callback-запрос учителя.</returns>
     private async Task<bool> HandleTeacherCallbackAsync(long chatId, string callbackData, int messageId)
     {
-      //TODO : Позже раскоментить
-      //var userData = CommonDataModel.GetUserById(chatId);
-      var userData = new UserModel(chatId, "TestName", "TestLastname", "email@mail.ru", UserRole.Teacher);
+      var userData = CommonUserModel.GetUserById(chatId);
       if (userData != null)
       {
         var teacher = new Teacher(userData.TelegramChatId, userData.FirstName, userData.LastName, userData.Email);
@@ -234,9 +231,7 @@ namespace TelegramBot
     /// <returns>Ответ на callback-запрос студента.</returns>
     private async Task<bool> HandleStudentCallbackAsync(long chatId, string callbackData, int messageId)
     {
-      //TODO : Позже раскоментить
-      // var userData = CommonDataModel.GetUserById(chatId);
-      var userData = new UserModel(chatId, "TestName", "TestLastname", "email@mail.ru", UserRole.Student);
+      var userData = CommonUserModel.GetUserById(chatId);
       if (userData != null)
       {
         var student = new Student(userData.TelegramChatId, userData.FirstName, userData.LastName, userData.Email);
